1b7ce33b328db30c74353dd572f06095639f7d69
xuyang <y.xu@neoclub.cn>
2020-10-14 19:05:56 +0800

    fix:.

--- demo/demo.js
+++ demo/demo.js
@@ -1,8 +1,31 @@
 const client = require('prom-client');
 const counter = new client.Counter({
     name: 'count',
-    help: 'help'
+    help: 'help',
+    labelNames: ['code', 'message'],
 });
-counter.inc(); // Inc with 1
+const gauge = new client.Gauge({
+    name: 'Gauge',
+    help: 'help',
+    labelNames: ['code', 'message'],
+});
+const gistogram = new client.Histogram({
+    name: 'gistogram',
+    help: 'help',
+    labelNames: ['code', 'message'],
+});
+const summary = new client.Summary({
+    name: 'Summary',
+    help: 'help',
+    labelNames: ['code', 'message'],
+});
+counter.inc({ code: 200, message: 'ok' }, 1); // Inc with 1
 counter.inc(10); // Inc with 10
+gauge.set(256)
+gistogram.observe({ code: 200 }, 0.3)
+summary.observe({ code: 211 }, 1)
+summary.observe({ code: 211 }, 2)
+summary.observe({ code: 211 }, 3)
+summary.observe({ code: 211 }, 4)
+summary.observe({ code: 211 }, 5)
 console.log(client.register.metrics())
--- demo/server.js
+++ demo/server.js
@@ -1,12 +1,32 @@
 var http = require("http");
 const client = require('prom-client');
+
 const counter = new client.Counter({
     name: 'count',
-    help: 'help'
+    help: 'help',
+    labelNames: ['code', 'message'],
+});
+const gauge = new client.Gauge({
+    name: 'Gauge',
+    help: 'help',
+    labelNames: ['code', 'message'],
+});
+const gistogram = new client.Histogram({
+    name: 'gistogram',
+    help: 'help',
+    labelNames: ['code', 'message'],
+});
+const summary = new client.Summary({
+    name: 'Summary',
+    help: 'help',
+    labelNames: ['code', 'message'],
 });
 http.createServer(function (request, response) {
     response.writeHead(200, { 'Content-Type': 'text/plain' });
-    counter.inc();
+    counter.inc({ code: 200, message: '' });
+    gauge.set(1)
+    gistogram.observe(Math.random() * 10)
+    summary.observe(Math.random())
     console.log(new Date())
     response.end(client.register.metrics());
 }).listen(8081);
