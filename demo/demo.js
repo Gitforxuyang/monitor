e421a57337324f50d275699912900618e079adb4
xuyang <y.xu@neoclub.cn>
2020-10-16 22:14:27 +0800

    feat:查看所有的namespace

--- demo/demo.js
+++ demo/demo.js
@@ -9,7 +9,7 @@ const gauge = new client.Gauge({
     help: 'help',
     labelNames: ['code', 'message'],
 });
-const gistogram = new client.Histogram({
+const histogram = new client.Histogram({
     name: 'gistogram',
     help: 'help',
     labelNames: ['code', 'message'],
@@ -19,13 +19,14 @@ const summary = new client.Summary({
     help: 'help',
     labelNames: ['code', 'message'],
 });
-counter.inc({ code: 200, message: 'ok' }, 1); // Inc with 1
-counter.inc(10); // Inc with 10
-gauge.set(256)
-gistogram.observe({ code: 200 }, 0.3)
-summary.observe({ code: 211 }, 1)
-summary.observe({ code: 211 }, 2)
-summary.observe({ code: 211 }, 3)
-summary.observe({ code: 211 }, 4)
-summary.observe({ code: 211 }, 5)
+// counter.inc({ code: 200, message: 'ok' }, 1); // Inc with 1
+// counter.inc(10); // Inc with 10
+// gauge.set(256)
+histogram.observe({ code: Math.random() }, 0.3)
+histogram.observe({ code: Math.random() }, 0.3)
+// summary.observe({ code: 211 }, 1)
+// summary.observe({ code: 211 }, 2)
+// summary.observe({ code: 211 }, 3)
+// summary.observe({ code: 211 }, 4)
+// summary.observe({ code: 211 }, 5)
 console.log(client.register.metrics())
